85d1b0a9ac00c74177609d6eb9ed9d572596b538
Alvaro Cardozo <alvaro_car6@hotmail.com>
2022-06-12 15:15:59 -0500

    initial main-tophead declaration

---
+++ src/app/components/template/topbar/main-topbar/main-topbar.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MainTopbarComponent } from './main-topbar.component';
+
+describe('MainTopbarComponent', () => {
+  let component: MainTopbarComponent;
+  let fixture: ComponentFixture<MainTopbarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MainTopbarComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MainTopbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/template/topbar/main-topbar/main-topbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-topbar',
+  templateUrl: './main-topbar.component.html',
+  styleUrls: ['./main-topbar.component.scss'],
+})
+export class MainTopbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
