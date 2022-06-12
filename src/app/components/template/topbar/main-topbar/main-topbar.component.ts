4de9b5e6b3845d5d6a8834ff796e7c2e8c96c971
Alvaro Cardozo <alvaro_car6@hotmail.com>
2022-06-12 17:43:14 -0500

    improve tophead

--- src/app/components/template/topbar/main-topbar/main-topbar.component.ts
+++ src/app/components/template/topbar/main-topbar/main-topbar.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-main-topbar',
   templateUrl: './main-topbar.component.html',
   styleUrls: ['./main-topbar.component.scss'],
+  standalone: true
 })
 export class MainTopbarComponent implements OnInit {
 
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { MainTopbarComponent } from 'src/app/components/template/topbar/main-topbar/main-topbar.component';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { HomeComponent } from './home.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MainTopbarComponent,
   ]
 })
 export class HomeModule { }
