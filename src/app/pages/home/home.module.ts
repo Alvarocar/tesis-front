acfd47f6888e14f65abe8b6933d06cd936bcff1e
Alvaro Cardozo <alvaro_car6@hotmail.com>
2022-06-14 17:58:29 -0500

    explain component in home page

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -5,12 +5,14 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { MainTopbarComponent } from 'src/app/components/template/topbar/main-topbar/main-topbar.component';
 import { PresentationComponent } from './children/presentation/presentation.component';
+import { ExplainComponent } from './children/explain/explain.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    PresentationComponent
+    PresentationComponent,
+    ExplainComponent
   ],
   imports: [
     CommonModule,
